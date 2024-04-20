7e4b1dd6e51d3c7290981251be7b3dd6487f05e4
mean <fixounet@free.fr>
2024-04-20 18:16:07 +0200

    sync

--- main_st7735_test.cpp
+++ main_st7735_test.cpp
@@ -9,15 +9,28 @@
 #include "simpler9341_priv.h"
 #include "st7735_priv.h"
 #include "opensans20.h"
-lnSpi9341          *ili;
-hwlnSPIClass *spi;
+#include "../private_include/lnSPI_priv.h"
 
+lnSpi9341          *ili;
+lnSPI *spi;
+void dummyFunc();
 
 const uint8_t no_data[1]={0};
 
 // screen is rotated by 90 degrees
 #define SCREEN_WIDTH    WW
 #define SCREEN_HEIGHT   HH
+
+#ifdef USE_RP2040
+  #define PIN_DC GPIO8
+  #define PIN_CS GPIO9
+  #define PIN_RST GPIO10
+
+#else
+  #define PIN_DC PB11
+  #define PIN_CS PB8
+  #define PIN_RST PB9
+#endif
 
 /**
  * 
@@ -30,17 +43,17 @@ void setup()
     // ili must be first
     //
 #define FAST 1
-    spi=new hwlnSPIClass(0,-1);    
+    spi=lnSPI::create(0,-1);    
     lnSPISettings transaction(FAST*36*1000*1000+(1-FAST)*500, SPI_MSBFIRST, SPI_MODE0,-1);
     spi->begin();
     
     
     ili=new lnSpi9341( SCREEN_HEIGHT, SCREEN_WIDTH,
                                     spi,        
-                                    PB11,       // DC/RS
-                                    PB8,       // CS 
-                                    PB9);  // Reset
-    spi->beginTransaction(transaction);    
+                                    PIN_DC,       // DC/RS
+                                    PIN_CS,       // CS 
+                                    PIN_RST);  // Reset
+    spi->set(transaction);    
     
     ili->init(st7735_data,NULL);  
     ili->forceChipId(CHIPID_ST7735);
@@ -62,19 +75,39 @@ void once()
    ili->print(0,SCREEN_HEIGHT/2,"RED");
    ili->print(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,"BLUE");
 }
+void twice()
+{
+   ili->fillScreen(BLACK);
+   ili->setFontSize(ili9341::SmallFont);
+   ili->setTextColor(ILI_WHITE, ILI_BLACK);
+   ili->square(ILI_GREEN,0,0,SCREEN_WIDTH/2,SCREEN_HEIGHT);
+   ili->square(ILI_BLACK,SCREEN_WIDTH/2,0,SCREEN_WIDTH/2,SCREEN_HEIGHT);
+   ili->print(0,SCREEN_HEIGHT/2,"RED");
+   ili->print(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,"BLUE");
+}
 
 /**
  * @brief 
  * 
  */
+extern LN_SPI_Registers *aspi0 ;
 void loop()
 {
+    bool one=true;
     once();
     while(1)
     {
+        if(one) once();
+          else twice();
+        one=!one;
         lnDigitalToggle(LN_SYSTEM_LED);
         //foo();
         lnDelayMs(1000);
+        dummyFunc();
     }
+}
+void dummyFunc()
+{
+  Logger("%x\n",aspi0->DATA);
 }
 //
